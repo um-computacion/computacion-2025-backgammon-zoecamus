9248c5629f5cdcfa84bbaa038301d661c11dadc7
Zoe <z.camus@alumno.um.edu.ar>
2025-09-01 21:40:49 -0300

    clase jugador, primera clase

--- core/player.py
+++ core/player.py
@@ -1,2 +1,12 @@
 class player:
-    ...
+
+    def __init__(self, name: str, color: str, direction: int):
+        assert color in ("white", "black"), "color inválido"
+        assert direction in (-1, 1), "dirección inválida"
+
+        self.__name__ = name
+        self.__color__ = color
+        self.__direction__ = direction
+
+    def __repr__(self):
+        return f"Player(name={self.__name__!r}, color={self.__color__}, dir={self.__direction__})"

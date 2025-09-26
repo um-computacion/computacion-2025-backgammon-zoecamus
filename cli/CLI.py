2feb458fd628a047ffb656b52cb57c66702d2058
Zoe <z.camus@alumno.um.edu.ar>
2025-09-26 12:46:37 -0300

    cli inicial

---
+++ cli/CLI.py
@@ -0,0 +1,7 @@
+from core.player import Player
+from core.dice import Dice
+from core.board import Board
+from core.game import Game
+
+def main():
+    print("=== ¡¡¡ Bienvenido a Backgammon !!! ===")
